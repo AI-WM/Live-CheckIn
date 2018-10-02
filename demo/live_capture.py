af331669d4ecd04b26ac3b7ee2371a5fc1c3e8eb
HHONG-WM <40476813+HHONG-WM@users.noreply.github.com>
2018-10-01 23:10:32 -0400

    style: PEP8

--- demo/live_capture.py
+++ demo/live_capture.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from cv2 import imshow
-from cv2 import flip
-from cv2 import cvtColor
-from cv2 import COLOR_BGR2GRAY
-
 from sys import path
 from os.path import abspath
 from os.path import join
 from os import pardir
 path.append(abspath(join(__file__, pardir, pardir, 'src')))
+
+from cv2 import imshow
+from cv2 import flip
+from cv2 import cvtColor
+from cv2 import COLOR_BGR2GRAY
 
 from vcapture import vcap
 
